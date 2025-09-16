5c1088083898264137737acf576a67e50ddc798f
Ronaldo Silva <ronaldosilvadev@hotmail.com>
2025-09-16 12:04:40 -0300

    Aula 16.09.25

---
+++ Aula 16.09.25/exercicioRonaldo-js/script.js
@@ -0,0 +1,24 @@
+let nome = 'Ronaldo';
+let idade = 32;
+
+alert('Olá, ' + nome +'! ' + 'Você tem ' + idade +' anos.');
+
+//window.document.write(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+// Substitua document.write por innerHTML
+document.body.innerHTML += `Olá, ${nome}! Você tem ${idade} anos.`;
+
+
+// Exercício 2 – Funções
+// Crie uma função que receba a idade e diga se a pessoa é maior ou menor de idade, usando alert()
+
+function verificaridade(idade) {
+    if (idade >= 18) {
+        alert(`Olá, você com ${idade} é maior de idade.`)
+    } 
+    else {
+        alert(`Olá, voce com ${idade} é menor de idade.` )
+    }
+}
+
+verificaridade(idade);
